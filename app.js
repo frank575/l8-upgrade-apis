fc98a439181a21f05df3970b7acc2667cf69ba4c
吳俊緯 <wcw850710@gmail.com>
2021-09-02 21:13:13 +0800

    init app

---
+++ app.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify')({ logger: true })
+const PORT = 9281
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+	return { hello: 'world' }
+})
+
+// Run the server!
+const start = async () => {
+	try {
+		await fastify.listen(PORT)
+		console.log(`Server is running in http://localhost:${PORT}`)
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+}
+
+start()
