92998b276c3a7b3ab8f0cedbc3f0e570875260a1
吳俊緯 <wcw850710@gmail.com>
2021-09-02 21:47:07 +0800

    test fastify cors

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')()
 const { appConfig } = require('./config')
 
 const PORT = appConfig[process.env.NODE_ENV].PORT
+
+fastify.register(require('fastify-cors'), {
+	origin: (origin, cb) => {
+		if (/localhost/.test(origin)) {
+			//  Request from localhost will pass
+			cb(null, true)
+			return
+		}
+		// Generate an error on other origins, disabling access
+		cb(new Error('Not allowed from ' + origin))
+	},
+})
 
 // Declare a route
 fastify.get('/api/home', async (request, reply) => {
