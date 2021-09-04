2220b96585371637c0a935c6fac803ca00dc4591
吳俊緯 <wcw850710@gmail.com>
2021-09-04 12:42:24 +0800

    feat 新增 users schema

--- app.js
+++ app.js
@@ -14,22 +14,29 @@ const slashStr = str => {
 	return str ? (/^\//.test(str) ? str : `/${str}`) : ''
 }
 
-const loopRegisterRoute = routes => {
-	for (const prefixPath in routes) {
-		const controllers = routes[prefixPath]
-		for (const controllersKey in controllers) {
-			const route = controllers[controllersKey]
-			fastify.route({
-				...route,
-				url: `${API_BASE_URL}/${prefixPath}${slashStr(route.url)}`,
-			})
-		}
-	}
-}
-
 const start = async () => {
 	try {
 		fastify.register(require('fastify-cors'), {})
+		fastify.register(require('fastify-swagger'), {
+			routePrefix: '/documentation',
+			exposeRoute: true,
+			swagger: {
+				info: {
+					title: '您好，接口',
+					description: 'by frank575',
+					version: '0.0.0',
+				},
+				externalDocs: {
+					url: 'https://hackmd.io/rSdxPsX9QieDwqZGixJ5GA?view',
+					description: '查閱題目此點此連結',
+				},
+				host: 'localhost:9281',
+				schemes: ['http'],
+				consumes: ['application/json'],
+				produces: ['application/json'],
+				tags: [{ name: 'users', description: '使用者' }],
+			},
+		})
 
 		await mongoose.connect(MONGODB_URI)
 		console.log('MongoDB connected...')
@@ -43,44 +50,76 @@ const start = async () => {
 			}),
 		)
 
-		const routes = {
-			users: {
-				get: {
-					method: 'GET',
-					url: '',
-					handler: async (req, reply) => {
-						try {
-							return await UserModel.find()
-						} catch (error) {
-							return error
-						}
-					},
-				},
-				add: {
-					method: 'POST',
-					url: '',
-					handler: async (req, reply) => {
-						try {
-							const user = new UserModel(req.body)
-							return user.save()
-						} catch (error) {
-							return error
-						}
-					},
-				},
-			},
-		}
+		fastify.register(
+			(fastify, opt, done) => {
+				fastify.register(
+					(fastify, opt, done) => {
+						fastify.route({
+							method: 'GET',
+							url: '/',
+							schema: {
+								tags: ['users'],
+								summary: '取得使用者列表',
+								response: {
+									200: {
+										type: 'array',
+										items: {
+											type: 'object',
+											properties: {
+												name: {
+													type: 'string',
+													nullable: true,
+													default: null,
+												},
+												username: { type: 'string' },
+											},
+										},
+									},
+								},
+							},
+							async handler() {
+								try {
+									return await UserModel.find()
+								} catch (error) {
+									return error
+								}
+							},
+						})
 
-		loopRegisterRoute(routes)
+						fastify.route({
+							method: 'POST',
+							url: '/',
+							schema: {
+								tags: ['users'],
+								summary: '新增使用者',
+							},
+							async handler(req) {
+								try {
+									const user = new UserModel(req.body)
+									return user.save()
+								} catch (error) {
+									return error
+								}
+							},
+						})
+						done()
+					},
+					{ prefix: '/users' },
+				)
+				done()
+			},
+			{ prefix: API_BASE_URL || '' },
+		)
 
 		await fastify.listen(PORT)
-		fastify.log.info(`server listening on http://localhost:${PORT}/`)
+		fastify.swagger()
+
+		console.log(`server listening on http://localhost:${PORT}/`)
+		fastify.log.info(`server listening on ${PORT}/`)
 	} catch (err) {
 		fastify.log.error(err)
 		process.exit(1)
 	}
 }
-
-console.log(PORT, API_BASE_URL)
 
 start()
